4d88c87f1b2572a2583d553c97320db75556082b
Kenta Murata <mrkn@mrkn.jp>
2016-10-09 14:33:23 +0900

    rspec

---
+++ spec/pycall/libpython_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+module PyCall
+  RSpec.describe LibPython do
+    describe '.Py_GetVersion' do
+      it "is available" do
+        expect(LibPython.Py_GetVersion()).to be_a(String)
+      end
+    end
+  end
+end
--- spec/pycall_spec.rb
+++ spec/pycall_spec.rb
@@ -4,8 +4,4 @@ describe PyCall do
   it "has a version number" do
     expect(PyCall::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
