f4671f29de8b7eb2e678562d6c63517f5f53ef17
Kenta Murata <mrkn@mrkn.jp>
2017-02-17 14:32:57 +0900

    Add PyCall.builtin

--- lib/pycall/init.rb
+++ lib/pycall/init.rb
@@ -4,6 +4,12 @@ module PyCall
     return if initialized
 
     PyCall::LibPython.Py_InitializeEx(0)
+
+    @builtin = LibPython.PyImport_ImportModule(PYTHON_VERSION < '3.0.0' ? '__builtin__' : 'builtins')
+  end
+
+  class << self
+    attr_reader :builtin
   end
 
   __initialize_pycall__
