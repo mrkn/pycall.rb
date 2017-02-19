070d9e1dd4dd67e553b01b02188bb28704ffc736
Kenta Murata <mrkn@mrkn.jp>
2017-02-19 22:29:47 +0900

    Support to_a and to_ary in PyCall::Tuple

--- lib/pycall/tuple.rb
+++ lib/pycall/tuple.rb
@@ -38,5 +38,17 @@ module PyCall
       value = Conversions.from_ruby(value)
       LibPython.PyTuple_SetItem(__pyobj__, index, value)
     end
+
+    def to_a
+      [].tap do |ary|
+        i, n = 0, length
+        while i < n
+          ary << self[i]
+          i += 1
+        end
+      end
+    end
+
+    alias to_ary to_a
   end
 end
