7dff4d9fdb577971ae6f491995da785e0eb122f2
Kenta Murata <mrkn@mrkn.jp>
2017-02-20 15:02:35 +0900

    Add utility functions

---
+++ lib/pycall/utils.rb
@@ -0,0 +1,29 @@
+module PyCall
+  module Utils
+    def int(pyobj)
+      @int ||= PyCall.eval('int')
+      @int.(pyobj)
+    end
+
+    def len(pyobj)
+      @len ||= PyCall.eval('len')
+      @len.(pyobj)
+    end
+
+    def str(pyobj)
+      @str ||= PyCall.eval('str')
+      @str.(pyobj)
+    end
+
+    def tuple(*args)
+      PyCall::Tuple[*args]
+    end
+
+    def type(pyobj)
+      @type ||= PyCall.eval('type')
+      @type.(pyobj)
+    end
+  end
+
+  extend Utils
+end
