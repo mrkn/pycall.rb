28566c872f90b8e1d3a467c00a0a0ac6bebe75d3
Kenta Murata <mrkn@mrkn.jp>
2017-02-25 13:07:03 +0900

    Refactoring

--- lib/pycall/iruby_helper.rb
+++ lib/pycall/iruby_helper.rb
@@ -3,16 +3,24 @@ require 'iruby'
 
 module PyCall
   module IRubyHelper
-    ::IRuby::Display::Registry.module_eval do
-      match do |obj|
-        obj.kind_of?(PyCall::PyObject) &&
-          PyCall.hasattr?(obj, :_repr_html_) &&
-          obj._repr_html_.kind_of?(PyCall::PyObject) &&
-          obj._repr_html_.kind_of?(PyCall::LibPython.PyMethod_Type)
-      end
-      format 'text/html' do |obj|
-        obj._repr_html_.()
-      end
+    private
+
+    def check_pyobject_respond_to__repr_html_(obj)
+      return false unless obj.kind_of? PyObject
+      return false unless PyCall.hasattr?(obj, :_repr_html_)
+      obj._repr_html_.kind_of? PyCall::LibPython.PyMethod_Type
     end
+  end
+end
+
+::IRuby::Display::Registry.module_eval do
+  extend PyCall::IRubyHelper
+
+  match do |obj|
+    check_pyobject_respond_to__repr_html_(obj)
+  end
+
+  format 'text/html' do |obj|
+    obj._repr_html_.()
   end
 end
