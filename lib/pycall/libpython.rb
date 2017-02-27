ea41942533a4473bb014a8c3a1b719426b9bc34c
Kenta Murata <mrkn@mrkn.jp>
2017-02-27 12:29:44 +0900

    attach_function PyObject_Type

--- lib/pycall/libpython.rb
+++ lib/pycall/libpython.rb
@@ -151,6 +151,7 @@ module PyCall
     attach_function :PyObject_Dir, [PyObject.by_ref], PyObject.by_ref
     attach_function :PyObject_Repr, [PyObject.by_ref], PyObject.by_ref
     attach_function :PyObject_Str, [PyObject.by_ref], PyObject.by_ref
+    attach_function :PyObject_Type, [PyObject.by_ref], PyTypeObject.by_ref
 
     # Bool
 
