33e5cc52bfa68a5ca934be5f77322c581674d00f
Kenta Murata <mrkn@mrkn.jp>
2017-03-07 21:36:41 +0900

    Add examples for a PyCall::Dict

--- spec/pycall/dict_spec.rb
+++ spec/pycall/dict_spec.rb
@@ -4,10 +4,25 @@ module PyCall
   describe Dict do
     subject { Dict.new('a' => 1, 'b' => 2, 'c' => 3) }
 
-    specify do
-      expect(subject['a']).to eq(1)
-      expect(subject['b']).to eq(2)
-      expect(subject['c']).to eq(3)
+    describe '#[]' do
+      it 'returns a value corresponding to a given key' do
+        expect(subject['a']).to eq(1)
+        expect(subject['b']).to eq(2)
+        expect(subject['c']).to eq(3)
+      end
+    end
+
+    describe '#[]=' do
+      it 'stores a given value for a given key' do
+        subject['a'] *= 10
+        expect(subject['a']).to eq(10)
+
+        subject['b'] *= 10
+        expect(subject['b']).to eq(20)
+
+        subject['c'] *= 10
+        expect(subject['c']).to eq(30)
+      end
     end
 
     describe '#has_key?' do
