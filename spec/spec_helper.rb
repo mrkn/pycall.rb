920e15a434d4da2cdc671110f6e6d8ff1158b0ab
Kenta Murata <mrkn@mrkn.jp>
2017-03-10 17:38:20 +0900

    Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,13 @@ require "pycall"
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__)) do |file|
   require file
 end
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.expose_current_running_example_as :example
+  config.filter_run_when_matching :focus
+  config.order = :random
+  config.seed = ENV['RSPEC_SEED'] if ENV['RSPEC_SEED']
+  config.profile_examples = true if ENV['RSPEC_PROFILING']
+end
