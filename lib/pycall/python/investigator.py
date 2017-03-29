bb7fcb8fae8a777ae2661789f01661871cadc669
Kenta Murata <mrkn@mrkn.jp>
2017-03-29 09:55:23 +0900

    Investigate multiarch configuration of python

--- lib/pycall/python/investigator.py
+++ lib/pycall/python/investigator.py
@@ -2,5 +2,6 @@ from distutils.sysconfig import get_config_var
 import sys
 for var in ('executable', 'exec_prefix', 'prefix'):
   print(var + ': ' + str(getattr(sys, var)))
-for var in ('VERSION', 'LIBRARY', 'LDLIBRARY', 'LIBDIR', 'PYTHONFRAMEWORKPREFIX'):
+print('multiarch: ' + str(getattr(sys, 'implementation', sys)._multiarch))
+for var in ('VERSION', 'LIBRARY', 'LDLIBRARY', 'LIBDIR', 'PYTHONFRAMEWORKPREFIX', 'MULTIARCH'):
   print(var + ': ' + str(get_config_var(var)))
