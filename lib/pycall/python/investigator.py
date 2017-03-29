429a59268d9db56ee89ebf8e76c07df194767d7b
Kenta Murata <mrkn@mrkn.jp>
2017-03-29 10:25:04 +0900

    Investigate INSTSONAME

--- lib/pycall/python/investigator.py
+++ lib/pycall/python/investigator.py
@@ -3,5 +3,5 @@ import sys
 for var in ('executable', 'exec_prefix', 'prefix'):
   print(var + ': ' + str(getattr(sys, var)))
 print('multiarch: ' + str(getattr(sys, 'implementation', sys)._multiarch))
-for var in ('VERSION', 'LIBRARY', 'LDLIBRARY', 'LIBDIR', 'PYTHONFRAMEWORKPREFIX', 'MULTIARCH'):
+for var in ('VERSION', 'INSTSONAME', 'LIBRARY', 'LDLIBRARY', 'LIBDIR', 'PYTHONFRAMEWORKPREFIX', 'MULTIARCH'):
   print(var + ': ' + str(get_config_var(var)))
