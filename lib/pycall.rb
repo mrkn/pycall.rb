049e730551c17212c6ea4430c95f4bbaa22ceaf7
Kenta Murata <mrkn@mrkn.jp>
2017-04-24 22:20:22 +0900

    Add an example for hash key

--- lib/pycall.rb
+++ lib/pycall.rb
@@ -1,6 +1,7 @@
 require "pycall/version"
 require "pycall/libpython"
 require "pycall/pyobject_wrapper"
+require "pycall/pyclass_wrapper"
 require "pycall/pyobject"
 require "pycall/pyerror"
 require "pycall/eval"
--- spec/pycall/pyobject_spec.rb
+++ spec/pycall/pyobject_spec.rb
@@ -78,5 +78,38 @@ PYTHON
         }.from(false).to(true)
       end
     end
+
+    describe 'for hash key' do
+      before do
+        PyCall.eval(<<PYTHON, input_type: :file)
+class TestClass:
+  def __init__(self, value):
+    self.value = value
+    pass
+
+  def __hash__(self):
+    return self.value % 2
+
+  def __eq__(self, other):
+    return (self.value % 2) == (other.value % 2)
+PYTHON
+      end
+
+      after do
+        PyCall.eval('del TestClass', input_type: :file)
+      end
+
+      specify do
+        po1 = PyCall.eval('TestClass(42)')
+        po2 = PyCall.eval('TestClass(43)')
+        po3 = PyCall.eval('TestClass(44)')
+        vo = Object.new
+        hash = {}
+        hash[po1] = vo
+        expect(hash[po1]).to be_equal(vo)
+        expect(hash[po2]).to be_equal(nil)
+        expect(hash[po3]).to be_equal(vo)
+      end
+    end
   end
 end
