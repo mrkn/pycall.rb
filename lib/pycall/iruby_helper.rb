72b7adedabbf34cc66d27bfa0e4fcc05572a55be
Kenta Murata <mrkn@mrkn.jp>
2017-09-09 16:08:33 +0900

    Fix iruby helpers
    - rewrite against the changes by PyCall version 1.0
    - matcher should return true when it is matched
    - formatters' priorities should be 1 because they must be checked before IRuby's default formatters

--- lib/pycall/iruby_helper.rb
+++ lib/pycall/iruby_helper.rb
@@ -5,21 +5,19 @@ module PyCall
   module IRubyHelper
     private
 
-    def check_pyobject_respond_to_format_method(obj, method_name)
-      return false unless obj.kind_of? PyObject
-      return false unless PyCall.hasattr?(obj, method_name)
-      PyCall.getattr(obj, method_name).kind_of? PyCall::LibPython.PyMethod_Type
+    def check_python_object_respond_to_format_method(obj, method_name)
+      return false unless obj.kind_of? PyObjectWrapper
+      return false unless obj.respond_to? method_name
+      true
     end
 
-    def register_pyobject_formatter(format_name, mime, priority_value=0)
+    def register_python_object_formatter(format_name, mime, priority_value=0)
       method_name = :"_repr_#{format_name}_"
       match do |obj|
-        check_pyobject_respond_to_format_method(obj, method_name)
+        check_python_object_respond_to_format_method(obj, method_name)
       end
       priority priority_value
-      format mime do |obj|
-        PyCall.getattr(obj, method_name).()
-      end
+      format mime, &method_name
     end
   end
 end
@@ -27,14 +25,14 @@ end
 ::IRuby::Display::Registry.module_eval do
   extend PyCall::IRubyHelper
 
-  register_pyobject_formatter :html, 'text/html'
-  register_pyobject_formatter :markdown, 'text/markdown'
-  register_pyobject_formatter :svg, 'image/svg+xml'
-  register_pyobject_formatter :png, 'image/png'
-  register_pyobject_formatter :jpeg, 'image/jpeg'
-  register_pyobject_formatter :latex, 'text/latex'
-  register_pyobject_formatter :json, 'application/json'
-  register_pyobject_formatter :javascript, 'application/javascript'
-  register_pyobject_formatter :pdf, 'application/pdf'
-  register_pyobject_formatter :pretty, 'text/plain', -1000
+  register_python_object_formatter :html, 'text/html', 1
+  register_python_object_formatter :markdown, 'text/markdown', 1
+  register_python_object_formatter :svg, 'image/svg+xml', 1
+  register_python_object_formatter :png, 'image/png', 1
+  register_python_object_formatter :jpeg, 'image/jpeg', 1
+  register_python_object_formatter :latex, 'text/latex', 1
+  register_python_object_formatter :json, 'application/json', 1
+  register_python_object_formatter :javascript, 'application/javascript', 1
+  register_python_object_formatter :pdf, 'application/pdf', 1
+  register_python_object_formatter :pretty, 'text/plain', -1000
 end
