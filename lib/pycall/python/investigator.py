d56b7dcac1785d6f3eb688d440c96e9ea489a7b1
Kenta Murata <mrkn@mrkn.jp>
2017-09-19 05:22:58 +0900

    Set PYTHONHOME when using anaconda or miniconda

--- lib/pycall/python/investigator.py
+++ lib/pycall/python/investigator.py
@@ -2,6 +2,7 @@ from distutils.sysconfig import get_config_var
 import sys
 for var in ('executable', 'exec_prefix', 'prefix'):
   print(var + ': ' + str(getattr(sys, var)))
+print('conda: ' + 'true' if 'conda' in sys.version else 'false')
 print('multiarch: ' + str(getattr(getattr(sys, 'implementation', sys), '_multiarch', None)))
 for var in ('VERSION', 'INSTSONAME', 'LIBRARY', 'LDLIBRARY', 'LIBDIR', 'PYTHONFRAMEWORKPREFIX', 'MULTIARCH'):
   print(var + ': ' + str(get_config_var(var)))
