9248966bc5997e02011fa1e0d84f8b91bcbc7b06
jouhlmann <josiasuhlmann@web.de>
2020-05-07 14:01:12 +0200

    skip rake build for truffelruby

--- ext/pycall/extconf.rb
+++ ext/pycall/extconf.rb
@@ -1,3 +1,16 @@
+# Building with TruffleRuby does not work / is not required
+# Seems like putting out an empty Makefile is the best solution to skip build
+# https://stackoverflow.com/questions/17406246/native-extensions-fallback-to-pure-ruby-if-not-supported-on-gem-install/50886432
+
+if RUBY_ENGINE == "truffleruby"
+  mfile = open("Makefile", "wb")
+  mfile.puts '.PHONY: install'
+  mfile.puts 'install:'
+  mfile.puts "\t" + '@echo "You are using Truffleruby, therefore skipping build of native extension"'
+  mfile.close
+  exit 0
+end
+
 require 'mkmf'
 
 create_makefile('pycall')
