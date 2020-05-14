0f09592159513748708a6e140ed76268ec7598b3
Stefan Reschke <stefan.reschke@outlook.de>
2020-05-14 12:11:45 +0200

    Add empty pycall interface for filling

--- lib/pycall/pycall_truffleruby.rb
+++ lib/pycall/pycall_truffleruby.rb
@@ -1,3 +1,49 @@
 module PyCall
+  def self.import_module(name)
+    Polyglot.eval('python', "import #{name}\n#{name}")
+  end
 
+  def builtins
+    @builtins ||= wrap_module(LibPython::API.builtins_module_ptr)
+  end
+
+  def callable?(obj)
+
+  end
+
+  def dir(obj)
+
+  end
+
+  def eval(expr, globals: nil, locals: nil)
+    Polyglot.eval('python', expr)
+  end
+
+  def exec(code, globals: nil, locals: nil)
+
+  end
+
+  def getattr(*args)
+
+  end
+
+  def hasattr?(obj, name)
+
+  end
+
+  def len(obj)
+
+  end
+
+  def sys
+
+  end
+
+  def tuple(iterable=nil)
+
+  end
+
+  def with(ctx)
+
+  end
 end
