c430f63c8feca74679c9b012a9ffc99eb70b6b49
jouhlmann <josiasuhlmann@web.de>
2020-07-15 23:04:05 +0200

    fix set / dict / tuple

--- lib/pycall/truffleruby/tuple.rb
+++ lib/pycall/truffleruby/tuple.rb
@@ -4,7 +4,7 @@ module PyCall
   class Tuple < PyEnumerable
 
     def initialize(*args)
-      super PyCall.builtins.tuple(args).__pyptr__#TODO: do without wrap->unwrap->wrap
+      super PyCall.builtins.tuple.new(args).__pyptr__#TODO: do without wrap->unwrap->wrap
     end
   end
 end
