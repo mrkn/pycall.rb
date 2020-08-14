7b7f9e2e2c1c3f693d2599549f2619b1dd360843
Stefan Reschke <stefan.reschke@outlook.de>
2020-08-14 16:40:14 +0200

    Fix another tuple test

--- lib/pycall/truffleruby/tuple.rb
+++ lib/pycall/truffleruby/tuple.rb
@@ -6,5 +6,13 @@ module PyCall
     def initialize(*args)
       super PyCall.builtins.tuple.new(args).__pyptr__#TODO: do without wrap->unwrap->wrap
     end
+
+    def is_a?(class1)
+      if class1 == PyCall.tuple
+        true
+      else
+        super.is_a?(class1)
+      end
+    end
   end
 end
